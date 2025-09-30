4dca8c6d055325c002e8d543450a917f8aeba7a2
Tejas Venugopal <tejas0908@gmail.com>
2025-10-01 01:43:23 +0530

    update

--- whatsapp-bot/src/app.ts
+++ whatsapp-bot/src/app.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import twilio from 'twilio';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//const accountSid = process.env.TWILIO_ACCOUNT_SID;
+//const authToken = process.env.TWILIO_AUTH_TOKEN;
+//const client = twilio(accountSid, authToken);
+
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+app.post('/webhook', (req, res) => {
+    console.log(req.body.Body);
+    const twiml = new twilio.twiml.MessagingResponse();
+    const message = twiml.message("Message received! Hello again from the Twilio Sandbox for WhatsApp.");
+    res.type('text/xml').send(message.toString());
 });
 
 app.listen(port, () => {
